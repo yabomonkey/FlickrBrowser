9a0b68dc1a9e739cd4f59c39e04d352c7c089cd5
Daniel <yabomonkey@gmail.com>
2022-01-18 16:44:18 -0800

    Additional work on GetRawData

--- app/src/main/java/yabomonkey/example/flickrbrowser/GetRawData.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/GetRawData.kt
@@ -2,6 +2,12 @@ package yabomonkey.example.flickrbrowser
 
 import android.os.AsyncTask
 
-class GetRawData : AsyncTask<String, Void, String>() {
+private const val TAG = "GetRawData"
 
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+class GetRawData : AsyncTask<String, Void, String>() {
+    private val downloadStatus = DownloadStatus.IDLE
 }
