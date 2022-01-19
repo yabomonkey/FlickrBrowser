8108c012b31f8a8433f6719de67a2db0a2ac4213
Daniel <yabomonkey@gmail.com>
2022-01-18 17:10:29 -0800

    configured override of onPostExecute

--- app/src/main/java/yabomonkey/example/flickrbrowser/GetRawData.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/GetRawData.kt
@@ -1,6 +1,10 @@
 package yabomonkey.example.flickrbrowser
 
 import android.os.AsyncTask
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
 
 private const val TAG = "GetRawData"
 
@@ -11,5 +15,39 @@ enum class DownloadStatus {
 class GetRawData : AsyncTask<String, Void, String>() {
     private val downloadStatus = DownloadStatus.IDLE
 
-    
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITALIZED
+            return "No URL Specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITALIZED
+                    "doInBackground: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IO Exception reading data: ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackground: Security Exception: Needs permission? ${e.message}"
+                } else {
+                    downloadStatus = DownloadStatus.ERROR
+                    "Unknown Error: ${e.message}"
+                }
+            }
+        }
+    }
+
+
 }
