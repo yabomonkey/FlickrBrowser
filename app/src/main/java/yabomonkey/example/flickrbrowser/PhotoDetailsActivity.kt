a495cbae3b0bf9a97894b932eb952ef7a997db87
Daniel <yabomonkey@gmail.com>
2022-01-21 17:03:25 -0800

    Commented out unnecessary autogenerated code

--- app/src/main/java/yabomonkey/example/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -22,9 +22,9 @@ class PhotoDetailsActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_photo_details)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val navController = findNavController(R.id.nav_host_fragment_content_photo_details)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -32,9 +32,9 @@ class PhotoDetailsActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_photo_details)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_photo_details)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
 }
