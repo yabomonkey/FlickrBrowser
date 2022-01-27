6637f74a696292d06deeaed837c4c9ec2c7117d5
Daniel <yabomonkey@gmail.com>
2022-01-27 15:19:48 -0800

    Config simple gesture detector for single and long

--- app/src/main/java/yabomonkey/example/flickrbrowser/RecyclerItemClickListener.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/RecyclerItemClickListener.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 private val TAG = "RecyclerItemClickListen"
@@ -17,11 +18,38 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    private val gestureDetector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, ".onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            return if(childView != null) {
+                Log.d(TAG, ".onSingleTapUp: calling listener.onItemClick")
+                listener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView))
+                true
+            } else {
+                false
+            }
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, ".onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+             if(childView != null) {
+                Log.d(TAG, ".onLongPress: calling listener.onItemLongClick")
+                listener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView))
+            }
+
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, ".onInterceptTouchEvent starts $e")
         val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, ".onInterceptTouchEvent returning $result")
         return result
 
     }
+
+
 
 }
