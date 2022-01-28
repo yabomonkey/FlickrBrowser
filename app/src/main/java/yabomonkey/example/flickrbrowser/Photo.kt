351701b9cb561ff739496fa27abc419f0fa1be1e
Dan <yabomonkey@gmail.com>
2022-01-27 23:22:20 -0800

    Config Photo object as Parcelizable

--- app/src/main/java/yabomonkey/example/flickrbrowser/Photo.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/Photo.kt
@@ -1,11 +1,14 @@
 package yabomonkey.example.flickrbrowser
 
+import android.os.Parcelable
 import android.util.Log
+import kotlinx.parcelize.Parcelize
 import java.io.IOException
 import java.io.ObjectStreamException
 import java.io.Serializable
 
-class Photo(var title: String, var author: String, var authorId: String, var link: String, var tags: String, var image: String) : Serializable {
+@Parcelize
+class Photo(var title: String, var author: String, var authorId: String, var link: String, var tags: String, var image: String) : Parcelable {
     companion object {
         private const val serialVersionUID = 1L
     }
