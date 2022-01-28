5bed8e72ef3f73f55e54ab1a6ef2d448946e0d58
Dan <yabomonkey@gmail.com>
2022-01-27 23:23:24 -0800

    Finished PhotoDetailsActivity pulling from intent

--- app/src/main/java/yabomonkey/example/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/yabomonkey/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.navigation.ui.AppBarConfiguration
+import com.squareup.picasso.Picasso
 import yabomonkey.example.flickrbrowser.databinding.ActivityPhotoDetailsBinding
 
 class PhotoDetailsActivity : BaseActivity() {
@@ -19,6 +20,16 @@ class PhotoDetailsActivity : BaseActivity() {
         setContentView(binding.root)
 
         activateToolbar(true)
+
+        val photo = intent.getParcelableExtra<Photo>(PHOTO_TRANSFER) as Photo
+
+        binding.mainContent.photoTitle.text = photo.title
+        binding.mainContent.photoTags.text = photo.tags
+        binding.mainContent.photoAuthor.text = photo.author
+        Picasso.get()
+            .load(photo.link)
+            .error(R.drawable.placeholder)
+            .into(binding.mainContent.photoImage)
     }
 
 
